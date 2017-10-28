48427d9a86c9dc83dbaf06bc9b72e351039287d8
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2017-10-28 12:48:43 +0200

    Eliminar la suscripción al observable

--- src/app/clientes/listado-clientes/listado-clientes.component.ts
+++ src/app/clientes/listado-clientes/listado-clientes.component.ts
@@ -1,27 +1,33 @@
 import { Cliente, Grupo } from './../cliente.model';
 import { ClientesService } from './../clientes.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-listado-clientes',
   templateUrl: './listado-clientes.component.html',
   styleUrls: ['./listado-clientes.component.css']
 })
-export class ListadoClientesComponent implements OnInit {
+export class ListadoClientesComponent implements OnInit, OnDestroy {
 
   clientes: Cliente[] = [];
   clientes$: Observable<Cliente[]>;
+  clientesSubscription: Subscription;
 
   constructor(private clientesService: ClientesService) { }
 
   ngOnInit() {
     this.clientes$ = this.clientesService.getClientes$();
-    this.clientes$.subscribe(clientes => this.clientes = clientes);
+    this.clientesSubscription = this.clientes$.subscribe(clientes => this.clientes = clientes);
   }
 
   doBorrarCliente(cliente: Cliente) {
     console.log('yeah!', cliente);
     this.clientesService.borrarCliente(cliente);
+  }
+
+  ngOnDestroy() {
+    this.clientesSubscription.unsubscribe();
   }
 }
